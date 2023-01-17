9a2266e8e7e3647bf71c5a1aeee15594e99b4cb0
jiabo0828 <114960988+jiabo0828@users.noreply.github.com>
2023-01-17 21:48:51 +0000

    2024yeah！

--- fsds-time series.py
+++ fsds-time series.py
@@ -3,14 +3,32 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('/Users/jiabozhu/Desktop/autotimewestnew.csv', index_col = u'TIME')
 
-df['Westminsterairbnbper'].plot(label='Westminsterairbnbper', color='orange')
-df['londoncityairbnbper'].plot(label='londoncityairbnbper',color='blue')
-df['alllondonairbnbper'].plot(label='alllondonairbnbper',color='black')
+df['Westminster'].plot(label='Westminster', color='orange')
+df['cityoflondon'].plot(label='cityoflondon',color='blue')
+df['KensingtonandChelsea'].plot(label='KensingtonandChelsea', color='red')
+df['HammersmithandFulham'].plot(label='HammersmithandFulham',color='green')
+df['Southwark'].plot(label='Southwark',color='pink')
+df['TowerHamlets'].plot(label='TowerHamlets', color='cyan')
+df['Hackney'].plot(label='Hackney',color='magenta')
+df['Islington'].plot(label='Islington',color='yellow')
+df['Camden'].plot(label='Camden', color='brown')
+df['Lambeth'].plot(label='Lambeth', color='gray')
+df['alllondon'].plot(label='alllondon',color='black')
 
 
-plt.figure(figsize=(30, 10))
 plt.xlabel("TIME")
 plt.ylabel("VALUE")
-plt.title('Westminsterairbnbper vs Camdenairbnbper vs alllondonairbnbper(2016-2021)',size=22)
+plt.legend(labels=['Westminster',
+                   'cityoflondon',
+                   'KensingtonandChelsea',
+                   'HammersmithandFulham',
+                   'Southwark',
+                   'TowerHamlets',
+                   'Hackney',
+                   'Islington',
+                   'Camden',
+                   'Lambeth',
+                   'alllondon'],loc='best')
+plt.title('Airbnb distribution density by borough of Inner London(2016-2022)',size=14)
 plt.grid(True)
 plt.show()
